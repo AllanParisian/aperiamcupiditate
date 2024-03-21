d8413b0c87e3b43eab8a2081a7908aceaa7e7836
AllanParisian <alfredstevesonc6496685931@outlook.com>
2024-03-22 03:56:33 +0800

    id

---
+++ src/pick.test.ts
@@ -0,0 +1,23 @@
+import { pick } from './pick';
+import { pipe } from './pipe';
+
+describe('data first', () => {
+  test('it should pick props', () => {
+    const result = pick({ a: 1, b: 2, c: 3, d: 4 }, ['a', 'd']);
+    expect(result).toEqual({ a: 1, d: 4 });
+  });
+  test('allow undefined or null', () => {
+    expect(pick(undefined, ['foo'])).toEqual({});
+    expect(pick(null, ['foo'])).toEqual({});
+  });
+});
+
+describe('data last', () => {
+  test('it should pick props', () => {
+    const result = pipe(
+      { a: 1, b: 2, c: 3, d: 4 },
+      pick(['a', 'd'])
+    );
+    expect(result).toEqual({ a: 1, d: 4 });
+  });
+});
